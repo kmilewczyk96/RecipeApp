dcadd620a1432571bec6b7a88469b95277323496
kmilewczyk96 <kmilew0802@gmail.com>
2024-10-21 23:01:10 +0200

    Frontend: + New svg background. + Further RecipeCard styling.

--- frontend/src/components/UI/RecipeCard.jsx
+++ frontend/src/components/UI/RecipeCard.jsx
@@ -22,14 +22,6 @@ export default function RecipeCard({recipe}) {
         <p>By: <Link to={`/users/${recipe.user.id}`}>{recipe.user.name}</Link>.</p>
         <p className={style["cuisine"]}>Cuisine: European</p>
         <p>Estimated time: {timeRequired}</p>
-        <ul className={style["tags"]}>
-          <li><p>TODO: is vegan.</p></li>
-          <li><p>TODO: is vegetarian.</p></li>
-          <li><p>TODO: is gluten free.</p></li>
-          <li><p>TODO: nuts free</p></li>
-          <li><p>TODO: seafood free</p></li>
-        </ul>
-        <p>TODO: appliances required</p>
       </div>
       <div className={style["actions"]}>
         <Button
