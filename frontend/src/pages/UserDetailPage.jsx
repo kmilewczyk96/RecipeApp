3c046d7fe9ea22ceb2eef1a7b97d5007f4dbc0d7
kmilewczyk96 <kmilew0802@gmail.com>
2024-12-10 12:16:40 +0100

    Frontend: + User profiles skeleton.

--- frontend/src/pages/UserDetailPage.jsx
+++ frontend/src/pages/UserDetailPage.jsx
@@ -1,14 +1,38 @@
-import {json} from "react-router-dom";
+import {json, useLoaderData, useSearchParams} from "react-router-dom";
 
 import {getToken} from "/src/util/auth-token.js";
+import style from "./layouts/UserDetailLayout.module.css";
+import ProfileDetail from "../components/user/ProfileDetail.jsx";
+import RecipeGrid from "../components/recipe/RecipeGrid.jsx";
+import {useQuery} from "@tanstack/react-query";
+import {fetchRecipes} from "../util/http.js";
 
 
 export default function UserDetailPage() {
+  const user = useLoaderData();
+  console.log(user)
+  const [searchParams] = useSearchParams();
+  const name = searchParams.get("name");
+  const {data, isLoading, isError, error} = useQuery({
+    queryKey: ["recipes", {userID: user.id, name: name}],
+    queryFn: ({signal}) => fetchRecipes({signal, userSuffix: user.id, name})
+  });
+
   return (
-    <></>
+    <div className={style["user-detail-layout"]}>
+      <ProfileDetail user={user}/>
+      {(data && !isLoading) && (
+        <RecipeGrid
+          addRecipe={false}
+          noRecipeMessage={"This user doesn't have any recipes yet."}
+          recipes={data}
+        />
+      )}
+    </div>
   );
 };
-export async function userDetailLoader({request, params}){
+
+export async function userDetailLoader({request, params}) {
   const userID = params["userID"] + "/";
   const response = await fetch("http://localhost:8000/api/user/users/" + userID, {
     method: "GET",
