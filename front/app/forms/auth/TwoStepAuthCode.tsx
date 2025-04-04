4a36211f1c8e733d774dbc58b2623f639ae0db98
kmilewczyk96 <kmilew0802@gmail.com>
2025-04-04 22:33:44 +0200

    Cosmetic changes for password fields, button improvements.

---
+++ front/app/forms/auth/TwoStepAuthCode.tsx
@@ -0,0 +1,10 @@
+import type {ReactElement} from "react";
+
+
+export default function TwoStepAuthCode(): ReactElement {
+  return (
+    <>
+    {/* TODO: implement 2step-auth. */}
+    </>
+  );
+}
